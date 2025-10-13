06984a0bcee416037f4e2774b9345dfe47eb28ae
seyfo32 <75393169+seyfo32@users.noreply.github.com>
2025-10-13 18:52:01 +0300

    Create JobListings & JobListing components with job limit, toggle button, and description truncate

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,12 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 <template>
 <Navbar/>
-<Hero title="Test Title" subTitle="Test Subtitle" />
+<Hero />
+<HomeCards/>
+<JobListings :limit="3" :show-button="true"/>
 </template>
