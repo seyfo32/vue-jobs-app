11ba45a0db15b9518712884775a19afec31f534d
seyfo32 <75393169+seyfo32@users.noreply.github.com>
2025-10-15 02:22:37 +0300

    Add job functionality with axios POST, dynamic routing and toast notifications

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const router = createRouter({
     history : createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,12 @@ const router = createRouter({
             path : '/jobs/:id',
             name:'job',
             component : JobView,
+
+        },
+         {
+            path : '/jobs/add',
+            name:'add-job',
+            component : AddJobView,
 
         },
         
